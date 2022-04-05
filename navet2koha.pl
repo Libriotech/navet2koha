04b0613c8c1440fdb01f38cdef385533574632c7
Magnus Enger <magnus@libriotech.no>
2022-04-05 09:15:50 +0200

    Say, not print to the log

--- navet2koha.pl
+++ navet2koha.pl
@@ -282,7 +282,7 @@ sub _process_borrower {
             # Check if Fornamn (firstname) needs to be reduced to Tilltalsnamn (preferred name)
             if ( $key eq 'firstname' && defined $config->{'use_tilltalsnamn'} && $config->{'use_tilltalsnamn'} == 1 ) {
                 $navet_value = Util::get_tilltalsnamn( $node->findvalue( './Personpost/Namn/Fornamn' ), $node->findvalue( './Personpost/Namn/Tilltalsnamnsmarkering' ) );
-                print $log $node->findvalue( './Personpost/Namn/Tilltalsnamnsmarkering' ) . ' ' . $node->findvalue( './Personpost/Namn/Fornamn' ) . ' => ' . $navet_value if $config->{'verbose'};
+                say $log $node->findvalue( './Personpost/Namn/Tilltalsnamnsmarkering' ) . ' ' . $node->findvalue( './Personpost/Namn/Fornamn' ) . ' => ' . $navet_value if $config->{'verbose'};
             }
 
             print $log $key . ' Koha="' . $borrower->$key . '" <=> Navet="' . $navet_value . '"' if $config->{'verbose'};
